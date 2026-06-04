297a9666798ee823be7dc7c6112053d3d415b4a4
santiago <sgrynwaldsikorski@itba.edu.ar>
2026-06-04 18:36:52 -0300

    Fix Vercel deployment

--- app/producto/[id]/page.jsx
+++ app/producto/[id]/page.jsx
@@ -1,4 +1,11 @@
 import App from '../../../src/App.jsx';
+import { productos } from '../../../src/data/productos.js';
+
+export function generateStaticParams() {
+    return productos
+        .flatMap(categoria => categoria.items)
+        .map(producto => ({ id: producto.id }));
+}
 
 export default function ProductoPage() {
     return <App />;
